89e827d3ac6497efdfb50c9bdb80bfbec3911b8e
jahidulislamkhan <mdjahidulislamkhans@gmail.com>
2020-12-01 02:25:47 +0600

    Implemented list books and deletion of these

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import './App.css';
+import Books from './Books/Books';
 
 class App extends Component {
 
+  state = {
+    books: [
+      {
+        id: 1,
+        name: 'Javascript',
+        price: 20
+      },
+      {
+        id: 2,
+        name: 'React',
+        price: 29
+      },
+      {
+        id: 3,
+        name: 'Redux',
+        price: 33
+      },
+      {
+        id: 4,
+        name: 'React Native',
+        price: 25
+      }
+    ]
+  }
+
+  deleteHandler = (id) => {
+    let newBooks = this.state.books.filter(book => book.id != id)
+    this.setState({
+      books: newBooks
+    })
+  }
+
   render() {
-    let style = {
-      fontSize: '30px',
-      fontFamily: 'Arial',
-      color: 'white',
-      textAlign: 'center'
-    }
     return (
       <div className="App">
-        <h1 style={ style }>CSS has been implemented here</h1>
+        <Books deleteHandler={ this.deleteHandler.bind(this) } books={ this.state.books } />
       </div>
     );
   }
