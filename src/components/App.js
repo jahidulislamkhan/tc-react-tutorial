426442dddc8b65c75d29cacd79680a66b8b5bafc
jahidulislamkhan <mdjahidulislamkhans@gmail.com>
2020-12-01 00:49:16 +0600

    Implemented CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Example from './Example';
 
 class App extends Component {
 
-  state = {
-    name: ''
-  }
-
-  inputHandler = (e) => {
-    this.setState({
-      name: e.target.value
-    })
-  }
-
   render() {
+    let style = {
+      fontSize: '30px',
+      fontFamily: 'Arial',
+      color: 'white',
+      textAlign: 'center'
+    }
     return (
       <div className="App">
-        <div className="container my-3">
-          <input type="text" onChange={ this.inputHandler } value={ this.state.name } className="form-input"/>&nbsp;
-          <button onClick={ (e) => console.log(e.target) } className="btn btn-primary">
-            Click Here
-          </button>
-          { this.state.name ? <p className="text-success">Hello { this.state.name }!</p> : '' }
-          <Example content='I am a Stateless Component' />
-        </div>
+        <h1 style={ style }>CSS has been implemented here</h1>
       </div>
     );
   }
